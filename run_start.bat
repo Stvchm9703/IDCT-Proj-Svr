3a58a0b58fbf643e8d785c1e6d953278778e3bc5
steven <stv.chm@gmail.com>
2020-03-14 15:03:01 +0800

    update

---
+++ run_start.bat
@@ -0,0 +1,2 @@
+start bin\_auth.exe start -c=config.yaml -m=test
+start bin\_roomstatus.exe start -c=config.yaml -m=test
---
+++ run_test.bat
@@ -0,0 +1,2 @@
+start cmd && go run build_cli\auth_server.go start -c=config.yaml 
+start cmd && go run build_cli\room_status.go start -c=config.yaml
